8885f648b421d2c5b5f86e94fd109acb32cc4e68
shukubota <kubotashu0421@gmail.com>
2022-07-17 02:11:43 +0900

    test追記

---
+++ src/main/main_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEnqueue(t *testing.T) {
+	t.Run("enqueue", func(t *testing.T) {
+		assert.Equal(t, 1, 1)
+	})
+}
