0bc51bb056530c36d66d39c6163830dfcee8a60f
shukubota <kubotashu0421@gmail.com>
2022-07-17 18:32:18 +0900

    test書く

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -1,12 +1,14 @@
 package main_test
 
 import (
+	"github.com/shukubota/amazonlinux2-sandbox/src/main"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEnqueue(t *testing.T) {
 	t.Run("enqueue", func(t *testing.T) {
-		assert.Equal(t, 1, 1)
+		err := main.Enqueue()
+		assert.NoError(t, err)
 	})
 }
