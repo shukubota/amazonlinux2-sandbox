723b77795fa0fefed4fe375139d2e1dcef7b0530
shukubota <kubotashu0421@gmail.com>
2022-07-28 13:12:31 +0900

    test修正

--- src/main/main_test.go
+++ src/main/main_test.go
@@ -2,19 +2,25 @@ package main_test
 
 import (
 	"github.com/shukubota/amazonlinux2-sandbox/src/main"
+	adaptor "github.com/shukubota/amazonlinux2-sandbox/src/sqs_adaptor"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEnqueueAndDeQueue(t *testing.T) {
-	t.Run("enqueue", func(t *testing.T) {
-		err := main.SendMessageToSQS("test message")
+	t.Run("enqueueしたメッセージをdequeueできメッセージを受け取れる", func(t *testing.T) {
+		s, err := adaptor.NewAdapter()
+		assert.NoError(t, err)
+
+		// sqsにenqueue
+		err = main.SendMessageToSQS(s, "生殺与奪の権を他人に握らせるな!!")
 		assert.NoError(t, err)
 
 		var message string
-		message, err = main.ReceiveMessageFromSQS()
+		// sqsからdequeue
+		message, err = main.ReceiveMessageFromSQS(s)
 		assert.NoError(t, err)
 
-		assert.Equal(t, message, "test message")
+		assert.Equal(t, message, "生殺与奪の権を他人に握らせるな!!")
 	})
 }
