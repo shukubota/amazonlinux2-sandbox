f6e4b89a58fdb1166a197c727cdf4a5ceda8fc7b
shukubota <kubotashu0421@gmail.com>
2022-10-31 22:33:56 +0900

    最低限のserver用意

--- src/server/server.go
+++ src/server/server.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("ssssss")
+	http.HandleFunc("/healthcheck", HealthCheck)
+	fmt.Println("listening on 9997")
+	http.ListenAndServe("0.0.0.0:9997", nil)
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "healthy v2")
 }
